c690b6c33ae99c965987388874a8467611bdee63
Lionel Chanson <github@lionelchanson.fr>
2018-01-08 14:21:07 +0100

    server(fix): fix response header for CORS

--- server.py
+++ server.py
@@ -11,6 +11,11 @@ class QuoteResource:
             ),
             'author': 'Grace Hopper'
         }
+        resp.status = falcon.HTTP_200
+        resp.append_header('Access-Control-Allow-Origin', '*')
+        resp.append_header('Allow', 'GET,OPTIONS')
+        resp.append_header('Access-Control-Allow-Headers', 'Content-Type')
+        resp.append_header('Access-Control-Max-Age', '86400')
 
         resp.media = quote
 
